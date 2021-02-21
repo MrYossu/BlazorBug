8a0476daeadb109f6a268083b2c143ce305cc68e
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-02-21 20:46:30 +0000

    Sample project

---
+++ Achim.Web/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Achim.Web {
+  public class Startup {
+    public Startup(IConfiguration configuration) =>
+      Configuration = configuration;
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddSession();
+      services.AddMemoryCache();
+      services.AddRazorPages();
+      services.AddServerSideBlazor().AddCircuitOptions(options => {
+        options.DetailedErrors = true;
+      });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      } else {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseSession();
+
+      app.UseRouting();
+      app.UseEndpoints(endpoints => {
+        endpoints.MapDefaultControllerRoute();
+        endpoints.MapControllers();
+        endpoints.MapBlazorHub();
+        endpoints.MapFallbackToAreaPage("/_Host", "General");
+      });
+    }
+  }
+}
